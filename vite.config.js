c8e20e6e4f5a182b57a929584c9a909e1bd2c206
cuentaparatodoxduwu7w7 <cuentaparatodoxduwu7w7@gmail.com>
2026-09-16 13:01:12 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: '/Math-Rush/',
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
